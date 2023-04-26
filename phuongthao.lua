bd94a36c6cf585dc17f02aaa2aebbc55045267e5
Zewi <ghitacuzimi@gmail.com>
2023-04-26 14:30:58 +0700

    Update phuongthao.lua

--- phuongthao.lua
+++ phuongthao.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 1537690962 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/iamz4ri/script/main/games/bss.lua"))()
 elseif game.PlaceId == 1417427737 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/iamz4ri/script/main/games/psx.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/iamz4ri/script/main/games/ms.lua"))()
 else
 	game.Players.LocalPlayer:Kick("Error loadng kocmoc.\n\nNo supported game found.\n\nPlease join a supported game.")
 end
