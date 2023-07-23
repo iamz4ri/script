2f8e602f6f99973cb851b01f85476102bf973d4c
Zewi <ghitacuzimi@gmail.com>
2023-07-23 15:52:08 +0700

    Tu bo moi thu'

--- phuongthao.lua
+++ phuongthao.lua
@@ -1,9 +1,5 @@
-if game.PlaceId == 1537690962 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/iamz4ri/script/main/games/bss.lua"))()
-elseif game.PlaceId == 1417427737 then
+if game.PlaceId == 1417427737 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/iamz4ri/script/main/games/ms.lua"))()
-elseif game.PlaceId == 6284583030 or game.PlaceId == 7722306047 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/iamz4ri/script/main/games/psx.lua"))()
 else
 	game.Players.LocalPlayer:Kick("Error loading.\n\nNo supported game found.\n\nPlease join a supported game.")
 end
